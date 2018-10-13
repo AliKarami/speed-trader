355d913808737f40217ad04532fd3b21b7c22ec7
Ali Karami <Ali.karami.m@gmail.com>
2018-10-13 23:59:45 +0330

    app.js sample

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 let init = require('./utils/init');
-const ee = require('./utils/eventEmiter');
 const Trade = require('./utils/trade');
 
 init().then(()=>{
-	try {
-		let trade = new Trade({
-			market: 'BTCUSDT',
-			entry: 44,
-			targets: [7000, 7100, 7200],
-			targetsShare: [30, 50, 20],
-			amount: 1,
-			// stopLoss: 1150,
-			stopLossPercent: 5,
-			trailing: true
-		});
-		trade.start();
-		// setInterval(() => {
-		// 	trade.pause();
-		// }, 10000);
-	} catch (e) {
-		console.error(e);
-	}
+	let trade = new Trade({
+		market: 'BTCUSDT',
+		entry: 44,
+		targets: [7000, 7100, 7200],
+		targetsShare: [30, 50, 20],
+		amount: 1,
+		// stopLoss: 1150,
+		stopLossPercent: 5,
+		trailing: true
+	});
+	return trade.start();
+}).then(()=>{
+	// setTimeout(() => {
+	// 	trade.pause();
+	// }, 60000);
+}).catch((e)=>{
+	console.error(e);
 });
